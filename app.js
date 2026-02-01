6777a5cc5f6a35ddbd918a137895c9493e762744
Matt Jones <mattjones@macbookpro.home>
2026-02-01 21:56:53 +0000

    Added CSS styling for index, login, register, services and traders pages

--- app.js
+++ app.js
@@ -37,7 +37,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname,'public')));
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
-app.use(express.static(path.join(__dirname,'public')));
 app.use(express.json());
 
 
